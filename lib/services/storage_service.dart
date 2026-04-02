e76e9f4f0fc6b49c03144f1eba42f84a2f60863f
maanav69420 <124855913+maanav69420@users.noreply.github.com>
2026-04-02 09:34:54 +0530

    Delete lib directory

--- lib/services/storage_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class StorageService {
-  static Future<void> saveUserId(String id) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("user_id", id);
-  }
-}
